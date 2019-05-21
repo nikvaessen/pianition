39c7e437a0e2dffd7a6c550e362d2b273c26a5e0
Nik <nikvaes@gmail.com>
2019-05-21 08:46:49 +0200

    memory freidnly

--- data/data_split.py
+++ data/data_split.py
@@ -88,6 +88,7 @@ def main():
         print("usage: python3 data_split path_to_storage")
 
     root_path = sys.argv[1]
+    print("saving data to", root_path)
 
     tr_paths, v_paths, t_paths = create_split_paths()
 
