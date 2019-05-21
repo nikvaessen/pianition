3ce8239f75093dfc237b5a2a5d51a0eb852d8fa5
Nik <nikvaes@gmail.com>
2019-05-21 08:29:23 +0200

    update window

--- pianition/data_util.py
+++ pianition/data_util.py
@@ -56,6 +56,7 @@ def _get_info() -> dict:
 
 def _load_sample(path: str) -> Tuple[int, np.ndarray]:
     sample = np.load(path, allow_pickle=True)['arr_0'].item()
+    print(sample)
 
     if each_column_is_mfcc:
         sample[1] = sample[1].transpose()
