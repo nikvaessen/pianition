595bef7ebcce663a3fb1ffe050a15cb315eecb89
Nik <nikvaes@gmail.com>
2019-05-29 00:15:20 +0200

    fix bug

--- data/create_splits.sh
+++ data/create_splits.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-python3 data_mfcc2split.py debug256 debug 128
+python3 data_mfcc2split.py debug128 debug 128
 python3 data_mfcc2split.py debug256 debug 256
 python3 data_mfcc2split.py debug512 debug 512
 python3 data_mfcc2split.py debug768 debug 768
 
-python3 data_mfcc2split.py full256 full 128
+python3 data_mfcc2split.py full128 full 128
 python3 data_mfcc2split.py full256 full 256
 python3 data_mfcc2split.py full512 full 512
 python3 data_mfcc2split.py full768 full 768
