98d2e9d79e608bde915f97a3f7189d82ebc64ed6
Nik <nikvaes@gmail.com>
2019-05-29 01:16:01 +0200

    fix more bugs at 1 am

--- experiments/network-generic.py
+++ experiments/network-generic.py
@@ -10,9 +10,9 @@ import keras
 from keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau
 from keras.optimizers import Adam, RMSprop
 
-
 import sys
-sys.path.insert(0,'..')
+
+sys.path.insert(0, '..')
 
 from pianition.data_util import load_dataset
 from pianition.keras_models import conv1d_gru, RNN
@@ -22,7 +22,7 @@ from pianition.keras_models import conv1d_gru, RNN
 
 
 def get_callbacks(checkpoint_name):
-    logDir = "./Graph/" + checkpoint_name +datetime.now().strftime("%H%M%S")+ "/"
+    logDir = "./Graph/" + checkpoint_name + datetime.now().strftime("%H%M%S") + "/"
     tb = TensorBoard(log_dir=logDir,
                      histogram_freq=2,
                      write_graph=True,
@@ -57,50 +57,52 @@ def trainer(network_name, dataset_path, epochs, batch_size, lr):
     train_x, train_y = dataset.get_train_full()
     val_x, val_y = dataset.get_val_full()
     input_shape = (train_x.shape[1], train_x.shape[2])
-    #Create your model
+    # Create your model
     network = eval(network_name)
-    print("Running ",network_name," with ", input_shape)
+    print("Running ", network_name, " with ", input_shape)
     train_model = network((train_x.shape[1], train_x.shape[2]))
-    #compile
+    # compile
     train_model.compile(loss='categorical_crossentropy',
                         optimizer=Adam(lr),
                         metrics=['accuracy'])
-    #Train the model
+    # Train the model
     # Better to change checkpoint name before run
     train_model.fit(train_x,
                     train_y,
                     batch_size,
                     epochs,
                     validation_data=(val_x, val_y),
-                    callbacks=get_callbacks(checkpoint_name=network_name+"_"+str(input_shape)))
+                    callbacks=get_callbacks(checkpoint_name=network_name + "_" + str(input_shape)))
 
 
 # In[4]:
 
 
-def run_exps(path = "../data/",dataset_type="full", epochs=1, batch_size=32, lr=0.001):
-    for subdir, dirs, files in os.walk(path):
-        for dir in dirs:
-            if(dataset_type in dir):
-                current_data_path = os.path.join(path, dir)
-                print(current_data_path)
-                _ = trainer("conv1d_gru", current_data_path, epochs, batch_size, lr)
-#                 tf.reset_default_graph()
-                keras.backend.clear_session()
-                _ = trainer("RNN", current_data_path, epochs, batch_size, lr)
-                keras.backend.clear_session()
+def run_exps(path="../data/", dataset_type="full", epochs=1, batch_size=32, lr=0.001):
+    current_data_path = os.path.join(path)
+    print(current_data_path)
+
+    trainer("conv1d_gru", current_data_path, epochs, batch_size, lr)
+
+    keras.backend.clear_session()
+
+    trainer("RNN", current_data_path, epochs, batch_size, lr)
+
+    keras.backend.clear_session()
 
 
 # In[5]:
 
 # full
 
-run_exps(path="/media/drive/data/", dataset_type='debug')
-run_exps(path="/media/drive/data/", dataset_type='full')
+run_exps(path="/media/drive/data/debug128", dataset_type='debug')
+run_exps(path="/media/drive/data/debug256", dataset_type='debug')
+run_exps(path="/media/drive/data/debug512", dataset_type='debug')
+run_exps(path="/media/drive/data/debug768", dataset_type='debug')
 
+run_exps(path="/media/drive/data/full128", dataset_type='full')
+run_exps(path="/media/drive/data/full256", dataset_type='full')
+run_exps(path="/media/drive/data/full512", dataset_type='full')
+run_exps(path="/media/drive/data/full768", dataset_type='full')
 
 # In[ ]:
-
-
-
-
